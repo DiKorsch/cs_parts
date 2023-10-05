9d22ee107c72845db6de93061266d04c1f7c7eb6
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2023-10-05 10:40:49 +0200

    removed unnecessary scripts

--- 01_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source 00_config.sh
-
-docker build -f Dockerfile.cupy_cuda11 --tag dikorsch/cupy-cuda110 .
-
-docker compose build $@ && docker compose config test_cupy
-docker compose run test_cupy && echo "Installation Ready"
--- 02_main.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source 00_config.sh
-
-docker-compose config
-docker-compose run --rm experiment $@
-# docker-compose up
